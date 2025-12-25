4b1f947e6125896de401fe07c1519d05f29720a7
Florian <florian@technotro.com>
2025-12-25 09:19:59 +0100

    set up basic unit tests

---
+++ contracts/test/Voting.t.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {Voting} from "../src/Voting.sol";
+
+contract Voting is Test {
+    Voting public voting;
+
+    function setUp() public {
+        voting = new Voting();
+        assertEq(voting.proposalCount(), 0);
+    }
+}
