dd4834a71f8917934cddf003bb46fb2a6166898f
Florian <florian@technotro.com>
2025-12-25 09:31:47 +0100

    another sanity test

--- contracts/test/Voting.t.sol
+++ contracts/test/Voting.t.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.8.20;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 import {Voting} from "../src/Voting.sol";
+import "openzeppelin/contracts/utils/Strings.sol";
 
 contract VotingTester is Test {
     Voting public voting;
@@ -16,5 +17,11 @@ contract VotingTester is Test {
         uint256 proposalId = voting.createProposal(deadline);
         assertEq(proposalId, 0);
         assertEq(voting.proposalCount(), 1);
+    }
+
+    function test_createProposal_past_deadline() public {
+        uint64 deadline = uint64(block.timestamp - 1 seconds);
+        vm.expectRevert("cannot create proposals with deadlines in the past");
+        voting.createProposal(deadline);
     }
 }
