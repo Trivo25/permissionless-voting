c114743198d53b062bed6730583dd7fb4617f9fb
Florian <florian@technotro.com>
2025-12-25 10:37:30 +0100

    set up tests for voting-tally

--- guests/tests/voting-tally.rs
+++ guests/tests/voting-tally.rs
@@ -1,20 +1,6 @@
-// Copyright 2024 RISC Zero, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 use alloy_primitives::U256;
 use alloy_sol_types::SolValue;
-use guests::IS_EVEN_ELF;
+use guests::VOTING_TALLY_ELF;
 use risc0_zkvm::{default_executor, ExecutorEnv};
 
 #[test]
@@ -27,7 +13,7 @@ fn proves_even_number() {
         .unwrap();
 
     // NOTE: Use the executor to run tests without proving.
-    let session_info = default_executor().execute(env, IS_EVEN_ELF).unwrap();
+    let session_info = default_executor().execute(env, VOTING_TALLY_ELF).unwrap();
 
     let x = U256::abi_decode(&session_info.journal.bytes).unwrap();
     assert_eq!(x, even_number);
@@ -44,5 +30,5 @@ fn rejects_odd_number() {
         .unwrap();
 
     // NOTE: Use the executor to run tests without proving.
-    default_executor().execute(env, IS_EVEN_ELF).unwrap();
+    default_executor().execute(env, VOTING_TALLY_ELF).unwrap();
 }
